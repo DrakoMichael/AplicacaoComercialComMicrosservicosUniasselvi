c8020775b37e4132558486af4ce9544a30fe9d39
Michael mello <Michaelkcms@gmail.com>
2025-07-24 14:25:08 -0300

    Revert "Upload aplicação finalizada"
    This reverts commit 33cb9e8edd6d8167c4b8b4045852ffc0d04ae8c8.

--- database-Service/dist/routes/routes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-const router = express.Router();
-import { listarProdutos, criarProduto, } from "../controller/produtoController.js";
-router.post("/", criarProduto);
-router.get("/listarProdutos", listarProdutos);
-export default router;
-//# sourceMappingURL=routes.js.map
--- frontend-Service/dist/service/frontService.js
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=frontService.js.map
